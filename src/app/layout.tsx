e3da71ab1e2020be0b9462c24df338ced3dec3fa
Abderrahman El Malki <woosenteur@gmail.com>
2026-07-03 23:52:57 +0100

    feat: meta google-adsense-account ca-pub-8287820739614627

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -96,6 +96,9 @@ export const metadata: Metadata = {
   alternates: {
     canonical: '/',
   },
+  other: {
+    'google-adsense-account': 'ca-pub-8287820739614627',
+  },
 };
 
 export default function RootLayout({
